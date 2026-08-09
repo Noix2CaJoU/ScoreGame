043e9db73575d1927d327177deee7b70f0e1ab65
LéoP4 <leopicoron@orange.fr>
2026-08-10 01:57:19 +0200

    fix service worker cache

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,10 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: [],
+        runtimeCaching: [],
+      },
       manifest: {
         name: 'ScoreGame',
         short_name: 'ScoreGame',
@@ -19,16 +23,8 @@ export default defineConfig({
         scope: '/ScoreGame/',
         start_url: '/ScoreGame/',
         icons: [
-          {
-            src: 'icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
+          { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'icon-512.png', sizes: '512x512', type: 'image/png' }
         ]
       }
     })
